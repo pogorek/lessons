6b70d95a96bf6ee25901a0c9203eba1e2e355d2e
Piotr Ogorek <pt.ogorek@gmail.com>
2022-06-05 00:45:12 +0200

    ch12

--- sweigart_automate/chapter12.py
+++ sweigart_automate/chapter12.py
@@ -4,3 +4,13 @@
 # webbrowser.open('https://inventwithpython.com/')
 
 # Downloading Files from the Web with the requests Module
+
+import requests
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(type(res))
+# <class 'requests.models.Response'>
+print(res.status_code == requests.codes.ok)
+# True
+print(len(res.text))
+# 178981
+print(res.text[:250])
